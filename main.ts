32b362acca50bdd079b2b39de4f95de83a0fd27e
FichtnerTobias <tobias.fichtner.024@pslib.cz>
2025-05-30 13:24:51 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
+radio.setGroup(21)
+radio.setFrequencyBand(25)
+radio.setTransmitSerialNumber(true)
+let id = control.deviceSerialNumber()
 
-radio.setGroup(1)
+
+
 let start = true
-
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     start = !start
     if (start) {
@@ -10,18 +14,17 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         basic.showIcon(IconNames.No)
     }
 })
-
 basic.forever(function () {
+    console.log(control.deviceSerialNumber())
+
     if (start) {
         let x = input.acceleration(Dimension.X)
         let y = input.acceleration(Dimension.Y)
-        console.log(`${x} ${y}`)
 
-        radio.sendValue("x", x)
-        radio.sendValue("y", y)
-
+        radio.sendValue("xy" + y, x)
         basic.pause(50)
-    } else if (!start){
+    } else if (!start) {
         radio.sendValue("stop", 0)
     }
-})
+}
+)
